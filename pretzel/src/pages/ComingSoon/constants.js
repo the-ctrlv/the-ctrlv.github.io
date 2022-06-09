0182563d44010b755329675136f9037075cde4a3
etozhunya <49534738+the-ctrlv@users.noreply.github.com>
2022-06-09 23:40:15 +0300

    Added all sections to the project

--- pretzel/src/pages/ComingSoon/constants.js
+++ pretzel/src/pages/ComingSoon/constants.js
@@ -1,7 +1,21 @@
+import bankLogo from '../../assets/images/bank.svg'
 import electronicsLogo from '../../assets/images/electronics.svg';
+import heartLogo from '../../assets/images/heart.svg';
+import hobeLogo from '../../assets/images/hobe.svg';
 import homeLogo from '../../assets/images/home.svg';
 import iphoneLogo from '../../assets/images/iphone.svg';
+import noDramaLogo1 from '../../assets/images/no-drama1.svg';
+import noDramaLogo2 from '../../assets/images/no-drama2.svg';
+import noDramaLogo3 from '../../assets/images/no-drama3.svg';
+import noDramaLogo4 from '../../assets/images/no-drama4.svg';
+import paymentsLogo from '../../assets/images/payments.svg';
+import ratesLogo from '../../assets/images/rate.svg';
+import scvLogo from '../../assets/images/scv.svg';
+import unicefLogo from '../../assets/images/unicef.svg';
 import vehicleLogo from '../../assets/images/vehicle.svg';
+import wagsLogo from '../../assets/images/wags.svg';
+import wwfLogo from '../../assets/images/wwf.svg';
+
 
 export const benefitsList = [
   {
@@ -24,4 +38,50 @@ export const benefitsList = [
     title: 'Phone',
     text: 'Your smartphone is essential. Cover it for just a few dollars a month.',
   },
+]
+
+export const howItWorksList = [
+  {
+    logo: ratesLogo,
+    text: 'Flat, low rates'
+  },
+  {
+    logo: paymentsLogo,
+    text: 'Quick and easy payments'
+  },
+  {
+    logo: bankLogo,
+    text: 'No more worrying about unexpected expenses'
+  },
+  {
+    logo: heartLogo,
+    text: 'Money left over goes to great causes'
+  }
+]
+
+export const noDramaList = [
+  {
+    logo: noDramaLogo1,
+    text: 'We don’t do high pressure sales calls'
+  },
+  {
+    logo: noDramaLogo2,
+    text: 'We certainly don’t send spooky letters to the elderly'
+  },
+  {
+    logo: noDramaLogo3,
+    text: 'We actually pay claims (rather than looking for excuses not to)'
+  },
+  {
+    logo: noDramaLogo4,
+    text: 'We’ll never make you traverse the dark forest – or send certified mail – to cancel your plan (in fact, you can do it in seconds online) '
+  }
+
+
+
+]
+
+export const pretzelMeansList = [
+  wagsLogo, wwfLogo, scvLogo, unicefLogo, hobeLogo
+
 ]
--- pretzel/src/pages/ComingSoon/index.js
+++ pretzel/src/pages/ComingSoon/index.js
@@ -4,7 +4,7 @@ import ComingSoonModal from './Modal';
 import AIGLogo from '../../assets/images/aig-logo.svg';
 import AllianzLogo from '../../assets/images/allianz-logo.svg';
 import Footer from '../../containers/Footer';
-import { benefitsList } from './constants';
+import { benefitsList, howItWorksList, noDramaList, pretzelMeansList } from './constants';
 
 import './style.scss';
 
@@ -53,7 +53,7 @@ function ComingSoon() {
                                         <div className='image-wrapper d-flex align-items-center'>
                                             <img src={benefit.logo} alt={benefit.title} className="d-block" />
                                         </div>
-                                        <span className='fw-bold mt-4 mt-lg-2 d-block'>Your {benefit.title}</span>
+                                        <span className='fw-bold mt-4 mt-lg-2 d-block list-title'>Your {benefit.title}</span>
                                         <p className='px-3'>{benefit.text}</p>
                                     </div>
 
@@ -68,7 +68,79 @@ function ComingSoon() {
                         </ul>
                     </div>
                 </section>
-                <section className='backed-by'>
+
+                <section className='how-it-works'>
+                    <div className='container-xl text-center'>
+                        <span className='title fw-bold d-block mb-3'>
+                            Here's how it will work
+                        </span>
+                        <span className='sub-title fw-bold d-block'>
+                            We run protection plans the way they should be run – as a service designed to save you money and make your life easier. You pay a low, flat monthly fee, and when something happens that’s covered, we foot the bill or make sure you get paid easily and quickly.
+                        </span>
+                        <ul className='d-flex flex-wrap flex-lg-nowrap justify-content-between'>
+                            {howItWorksList.map((howItWorks, index) => (
+                                <li key={index} className='how-it-works__list text-center col-12 col-md-6 
+                                col-lg-3 mb-5 d-flex flex-column justify-content-between'>
+                                    <div className='image-wrapper d-flex align-items-center'>
+                                        <img src={howItWorks.logo} alt={howItWorks.title} className="d-block" />
+                                    </div>
+                                    <p className='px-3 fw-bold mt-3 list-title'>{howItWorks.text}</p>
+                                </li>
+                            ))}
+                        </ul>
+
+                    </div>
+                </section>
+
+                <section className='no-drama position-relative colored'>
+                    <div className='container-xl text-center'>
+                        <span className='title fw-bold d-block mb-3'>
+                            No more drama
+                        </span>
+                        <span className='sub-title fw-bold d-block'>
+                            We know what you’ve heard about extended warranties, but that’s not us. We’re here because, frankly?
+                            Extended warranties are broken, and we’re here to fix them. Simple as that.
+                        </span>
+                        <ul className='d-flex flex-wrap flex-lg-nowrap justify-content-between'>
+                            {noDramaList.map((noDrama, index) => (
+                                <li key={index} className='no-drama__list text-center col-12 col-md-6
+                                    col-lg-3 mb-5'>
+                                    <div className='image-wrapper d-flex align-items-center'>
+                                        <img src={noDrama.logo} alt={noDrama.title} className="d-block" />
+                                    </div>
+                                    <p className='px-3 fw-bold mt-5 list-title'>{noDrama.text}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </section>
+
+                <section className='pretzel-means'>
+                    <div className='container-xl text-center'>
+                        <span className='title fw-bold d-block mb-3'>
+                            Pretzel means coverage with a heart
+                        </span>
+                        <span className='fw-bold d-block my-3'>
+                            (we’re a soft, warm pretzel)
+                        </span>
+                        <span className='sub-title fw-bold d-block pxx-3 px-lg-5'>
+                            When we say our mission is making life better through better protection,
+                            we mean making the world better too. That’s why we make regular donations to charities chosen by Pretzel customers like you.
+                        </span>
+                        <ul className='d-flex flex-wrap flex-lg-nowrap justify-content-between mt-5'>
+                            {pretzelMeansList.map((pretzelMeans, index) => (
+                                <li key={index} className='pretzel-means__list text-center d-flex justify-content-center col-12 col-md-2 mb-5'>
+                                    <div className='image-wrapper h-100 d-flex align-items-center'>
+                                        <img src={pretzelMeans} alt={pretzelMeans} className="d-block" />
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </section>
+
+
+                <section className='backed-by colored'>
                     <div className='container-xl text-center'>
                         <span className='title fw-bold d-block'>Backed by the best</span>
                         <p className='fw-bold px-4 px-xl-2'>Pretzel has great extended protection plans, backed by top financial institutions like Allianz, AIG, and A+ financial. </p>
