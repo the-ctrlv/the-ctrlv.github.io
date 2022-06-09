1ef9409ae39f2d58f97e04d2b9ea05537943b029
etozhunya <49534738+the-ctrlv@users.noreply.github.com>
2022-06-09 12:01:51 +0300

    Footer: styled completely, added responsiveness

--- pretzel/src/containers/Footer/index.js
+++ pretzel/src/containers/Footer/index.js
@@ -12,10 +12,11 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="container-xl">
-                <ul className='d-flex justify-content-between pt-5'>
-                    <li>
+                <ul className='d-flex flex-column flex-md-row justify-content-between flex-md-wrap flex-lg-nowrap pt-5'>
+                    <li className='mt-4 mt-md-0 mb-5 mb-lg-0 text-center text-md-start order-last 
+                    order-md-first col-md-6 col-lg-auto'>
                         <img src={pretzelLogo} alt="Pretzel Logo" className='mb-4' />
-                        <div className='d-flex justify-content-between'>
+                        <div className='footer__logo-container d-flex justify-content-between col-6 col-lg-12'>
                             <a href='/'>
                                 <img src={facebookLogo} alt="Facebook Logo" />
                             </a>
@@ -30,19 +31,19 @@ function Footer() {
                             </a>
                         </div>
                     </li>
-                    <li>
+                    <li className='mb-4 col-md-6 col-lg-auto d-flex d-lg-block order-md-2 order-lg-0'>
                         <a href='/' className='d-block fw-bold'>
                             Plans
                         </a>
-                        <a href='/' className='d-block fw-bold'>
+                        <a href='/' className='d-block fw-bold mx-3 mx-lg-0'>
                             FAQ
                         </a>
                         <a href='/' className='d-block fw-bold'>
                             Login
                         </a>
                     </li>
-                    <li className='col-4'>
-                        <span className='fw-bold'>Services</span>
+                    <li className='col-md-4 mb-4 col-md-6 col-lg-4'>
+                        <span className='fw-bold user-select-none'>Services</span>
                         <ul className="d-flex flex-wrap">
                             {serviceLinks.map((link, index) => (
                                 <li key={index} className='w-50'>
@@ -51,19 +52,19 @@ function Footer() {
                             ))}
                         </ul>
                     </li>
-                    <li className='stay-in-touch-container p-2 pt-4 rounded'>
+                    <li className='stay-in-touch-container p-2 pt-4 rounded col-md-6 col-lg-auto order-4'>
                         <span className='fw-bold d-block'>Lets stay in touch!</span>
                         <p className='my-2'>Subscribe our newsletter</p>
                         <form className='d-flex form-group footer__form rounded'>
-                            <input type="text" placeholder="Enter email" className='pretzel-input dark' />
+                            <input type="text" placeholder="Enter email" className='pretzel-input dark flex-grow-1' />
                             <button type="submit" className='btn'>
                                 <img src={telegramLogo} alt="Submit Telegram Logo" />
                             </button>
                         </form>
                     </li>
                 </ul>
-                <div className='copyright w-100 text-center mx-3 mt-5'>
-                    <p className='pt-4 pb-5'>© Copyright 2021 Pretzel</p>
+                <div className='copyright w-100 text-center mt-4'>
+                    <p className='pt-4 pb-5 user-select-none'>© Copyright 2021 Pretzel</p>
                 </div>
             </div>
         </footer>
