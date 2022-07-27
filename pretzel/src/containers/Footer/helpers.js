0e76b2fec687168fcf171538a2d4db84196136d1
etozhunya <49534738+the-ctrlv@users.noreply.github.com>
2022-07-27 19:01:20 +0300

    budbo template added

--- pretzel/src/containers/Footer/helpers.js
+++
@@ -1,3 +0,0 @@
-export const serviceLinks = [
-    'About', 'Transparency', 'Newsroom', 'Giving back', 'Legal Stuff', 'Terms of services', 'Join the team', 'Contact Us'
-]
--- pretzel/src/shared/functions.js
+++
@@ -1,5 +0,0 @@
-export const getClassNames = (...args) =>
-  args.filter(Boolean).join(' ');
-
-export const mqMin = (breakpoint) => `screen and (min-width: ${breakpoint}px)`;
-export const mqMax = (breakpoint) => `screen and (max-width: ${breakpoint}px)`;
